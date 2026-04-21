288440030b53be895b20920057dea8cd8a633550
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2026-04-21 11:25:50 +0100

    Add initial package.json for server with dependencies

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const cors = require('cors');
+app.use(cors)
+app.use(express.json());
+
+const serverPort = process.env.PORT || 5643
+
+app.get('/', (req,res)=>{
+    res.status(201).json({'message': "I am running fine"})
+})
+
+app.listen(serverPort, ()=>{
+    console.log(`Lift off!!! server is running at ${serverPort}`);
+})
